74c173fbab8e2f0d746b56ba6343701d17b4d8f9
takayasu.nasu <taka.beckham@gmail.com>
2020-12-08 13:28:19 -0800

    Add Auto Layout with Story Bord.

---
+++ AutoLayoutStarterWithStoryBord/AutoLayoutStarterWithStoryBord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutStarterWithStoryBord
+//
+//  Created by 那須毅康 on 2020/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
